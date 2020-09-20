c8212689b74b2f0988380ac9a3c51a0ad5b049ab
Marc Schröder <45717371+Marc5214@users.noreply.github.com>
2020-09-20 12:36:03 +0200

    Update the_code.py

--- the_code.py
+++ the_code.py
@@ -1,1 +1,26 @@
-# placeholder
+import not_compiled, base64, tensorflow
+Dir = not_compiled.encoded
+print(Dir)
+nicht = not_compiled.cipher
+print(nicht)
+gefaellt = base64.b64encode
+print(gefaellt)
+moeglichkeit = 12
+class Moeglichkeit:
+    def __init__(self):
+        self.variable = None
+        self.train = 666
+tensorflow = Moeglichkeit()
+"""Lokalisieren = not_compiled.cipher.decrypt(base64.b64decode(not_compiled.encoded))""" #DEPRECATED
+#print(decoded)
+for du in range(moeglichkeit):
+    if du >= 15:
+        print(du)
+    else:
+        tensorflow.variable = "grosscomputer"
+def lokalisieren(lorem, ipsum):
+    print(tensorflow.variable)
+    return lorem + ipsum
+ergebnis = lokalisieren(4, 2)
+ergebnis += tensorflow.train
+#print(ergebnis, Lokalisieren)
